24b5a81e3a3719e1d148e5edfd6745077726ccf5
Emiliano López <emilianolch@gmail.com>
2023-06-30 20:55:30 -0300

    models annotated

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,5 +1,28 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: donations
+#
+#  id                  :bigint           not null, primary key
+#  net_received_amount :float            default(0.0), not null
+#  payment_data        :json
+#  payment_status      :integer          default("not_committed"), not null
+#  total_paid_amount   :float            default(0.0), not null
+#  uuid                :string
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  user_id             :bigint           not null
+#
+# Indexes
+#
+#  index_donations_on_user_id  (user_id)
+#  index_donations_on_uuid     (uuid)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 class Donation < ApplicationRecord
   belongs_to :user
 
--- spec/models/donation_spec.rb
+++ spec/models/donation_spec.rb
@@ -1,9 +1,36 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: donations
+#
+#  id                  :bigint           not null, primary key
+#  net_received_amount :float            default(0.0), not null
+#  payment_data        :json
+#  payment_status      :integer          default("not_committed"), not null
+#  total_paid_amount   :float            default(0.0), not null
+#  uuid                :string
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  user_id             :bigint           not null
+#
+# Indexes
+#
+#  index_donations_on_user_id  (user_id)
+#  index_donations_on_uuid     (uuid)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 require "rails_helper"
 
-RSpec.describe Donation, type: :model do
+RSpec.describe Donation do
   describe "associations" do
     it { is_expected.to belong_to(:user) }
+  end
+
+  describe "enums" do
+    it { is_expected.to define_enum_for(:payment_status) }
   end
 end
