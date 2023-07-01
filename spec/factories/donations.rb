f4dfe38d887434315cf270e8fc010756e4101d6b
Emiliano López <emilianolch@gmail.com>
2023-06-30 23:05:39 -0300

    testing

--- spec/factories/donations.rb
+++ spec/factories/donations.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # == Schema Information
 #
 # Table name: donations
@@ -24,9 +26,6 @@
 FactoryBot.define do
   factory :donation do
     user { nil }
-    uuid { "MyString" }
-    total_paid_amount { 1.5 }
-    net_received_amount { 1.5 }
-    data { "" }
+    uuid { SecureRandom.uuid }
   end
 end
--- spec/requests/payments_spec.rb
+++ spec/requests/payments_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "rails_helper"
+require "mercadopago"
 
 RSpec.describe "Payments", type: :request do
   let(:user) { create(:user) }
@@ -51,6 +52,47 @@ RSpec.describe "Payments", type: :request do
     it "sets a flash alert" do
       get pending_payments_path
       expect(flash[:alert]).to eq(I18n.t("payments.pending"))
+    end
+  end
+
+  describe "notification" do
+    before do
+      allow_any_instance_of(Mercadopago::SDK) # rubocop:disable RSpec/AnyInstance
+        .to receive_message_chain(:payment, :get) # rubocop:disable RSpec/MessageChain
+        .and_return({
+          response: {
+            "external_reference" => donation.uuid,
+            "status" => "approved",
+            "transaction_amount" => 10,
+            "transaction_details" => {
+              "net_received_amount" => 9.25,
+            },
+          },
+        })
+
+      sign_out user
+    end
+
+    let!(:donation) { create(:donation, user: user) }
+    let(:params) do
+      {
+        type: "payment",
+        data: {
+          id: "123",
+        },
+      }
+    end
+
+    it "returns http success" do
+      post notification_payments_path, params: params, as: :json
+      expect(response).to have_http_status(:success)
+    end
+
+    it "updates the donation" do
+      expect { post notification_payments_path, params: params, as: :json }
+        .to change { donation.reload.total_paid_amount }.to(10)
+        .and change { donation.reload.net_received_amount }.to(9.25)
+        .and change { donation.reload.payment_status }.to("approved")
     end
   end
 end
